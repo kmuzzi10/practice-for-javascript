5cdabb2cab050bc5f58bd7029979a618ef7bac37
kmuzzi10 <kmuzammil12546@gmail.com>
2023-02-03 00:19:05 +0500

    practice more

---
+++ practice/lab6/api.js
@@ -0,0 +1,27 @@
+/*
+var num =10
+num++
+if (num==10){
+    console.log('hello')
+}
+else if (num==11){
+    console.log('write')
+}
+else{
+    console.log('ot')
+}
+*/
+
+var num = 'hello'
+num = 'kan'
+switch(num){
+    case 'hello':
+    console.log('hello')
+    break;
+    case 'kan':
+        console.log('pi')
+        break;
+        case 'ban':
+            console.log('print')
+            break;
+}
