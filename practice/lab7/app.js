573fae9436885b6fc6a21af514e5199cf2013b2e
kmuzzi10 <kmuzammil12546@gmail.com>
2023-02-03 01:33:19 +0500

    much practice

---
+++ practice/lab7/app.js
@@ -0,0 +1,45 @@
+// var student = ['muzammil','hassam','zainab']
+// var score = [200,400,300]
+
+// for(let i = 0;i<score.length;i++){
+//     if(i==0){
+//        var percentage1 = score[0]*100/500
+//     }
+//     else if(i==1){
+//         var percentage2 = score[1]*100/500
+//     }
+//     else if(i==2){
+//         var percentage3 = score[2]*100/500
+//     }
+//     else{
+//         console.log('ndbn')
+//     }
+// }
+// for(let i = 0;i<=2;i++){
+//     if(i==0){
+//         console.log('score of '+student[0]+' is '+score[0]+' and '+percentage1)
+//     }
+//     else if(i==1){
+//         console.log('score of '+student[1]+' is '+score[1]+' and '+percentage2)
+//     }
+    
+//     else if(i==2){
+//         console.log('score of '+student[2]+' is '+score[2]+' and '+percentage3)
+//     }
+//     else{
+//         console.log('cyhdcy')
+//     }
+// }
+
+
+var arr = []
+
+var a=4
+for(let i = 0 ;i<a;i++){
+    if(i==0)
+    arr[a] = prompt('enter 5 values')
+    }
+    for(let i =0;i<arr.length;i++){
+        document.write(arr[a])
+    }
+
