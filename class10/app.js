7b5ec26d7cccbb230b71dbc2a8b1fd9df974eb17
kmuzzi10 <kmuzammil12546@gmail.com>
2023-02-06 00:15:11 +0500

    practice more

---
+++ class10/app.js
@@ -0,0 +1,57 @@
+// function poo(){
+//     document.getElementById('abc').className+='big font'
+//     // document.getElementById('abc').style.backgroundColor = 'red'
+// }
+// //plus equal made class that new classes also enter
+
+//dates topic
+//first method
+
+// var date = new Date()
+// var a = date.getDate
+// var s = a.toString()
+// var copy = s.slice(0,4)    //it do in numbers for example sun = 0 if you want to do in string do method to string 
+// console.log(copy)
+
+// var abc = date.toString()
+// var copy = abc.slice(0,3)
+// console.log(copy)
+
+// var date = new Date('18 feb , 2002')
+// var date1 = date.getTime()
+// console.log(date1)
+
+// var user = prompt('enter you date of birth')
+// var milisecond = new Date()
+// var getmilisec = milisecond.getTime() //milisecond count from jan 1970
+// var secondmili = new Date(user)
+// var secondmili2 = secondmili.getTime() //milisecond count from 18 feb 2002
+// var result = getmilisec-secondmili2;
+// var formula = result / (1000*60*60*24*365)
+
+// console.log(Math.floor(formula))
+
+
+
+//taget by tagname
+
+// function alld(){
+//   var abc =    document.getElementsByTagName('p')
+// //   abc[0] .style.color = 'red'
+// for(var i = 0 ;i<abc.length;i++){
+//     abc[i].style.color = 'red'
+// }
+
+// }
+
+// function alld(){
+//   var a =   document.getElementById('abc')
+//  var b =  a .getElementsByTagName
+//  for(let i = 0;i<b.length;i++){
+    
+//  }
+
+
+// }
+
+
